48f2b3c989e43c408f8dd7e3b3c4d9f8eb47e2ef
PsychicManlab <omer123k@gmail.com>
2020-06-17 18:03:11 +0300

    https

--- herolo/src/model/models.js
+++ herolo/src/model/models.js
@@ -5,7 +5,7 @@ const apikey = 'sTnVnt0Gy2gadVM6E6kxKGNcDoDfszCx'
 export function getLocations(locationString) {
     return axios({
         method: 'get',
-        url: `http://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apikey}&q=${locationString}`,
+        url: `https://dataservice.accuweather.com/locations/v1/cities/autocomplete?apikey=${apikey}&q=${locationString}`,
     }).then((response, error) => {
         var results = response.data
         return results
@@ -15,7 +15,7 @@ export function getLocations(locationString) {
 export function getCurrentCondition(locationKey) {
     return axios({
         method: 'get',
-        url: `http://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apikey}&details=true`,
+        url: `https://dataservice.accuweather.com/currentconditions/v1/${locationKey}?apikey=${apikey}&details=true`,
     }).then((response, error) => {
         var results = response.data
         return results
@@ -25,7 +25,7 @@ export function getCurrentCondition(locationKey) {
 export function getFiveDayWeather(locationKey) {
     return axios({
         method: 'get',
-        url: `http://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apikey}&metric=true`,
+        url: `https://dataservice.accuweather.com/forecasts/v1/daily/5day/${locationKey}?apikey=${apikey}&metric=true`,
     }).then((response, error) => {
         var results = response.data.DailyForecasts
         return results
